90a004e5b377871581227a91ab7e98360113d574
[Ahmet Ünsal] <[unsalahmet1998@outlook.com]>
2023-03-27 17:45:03 +0300

    url changed

--- Client/src/requestMethods.js
+++ Client/src/requestMethods.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "https://localhost:443/api/";
+const BASE_URL = "https://clothing-shop-ahmet-api.onrender.com/api/";
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
 });
