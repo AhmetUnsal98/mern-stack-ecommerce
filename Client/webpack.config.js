84fc619fc385a2124628d303eb3375b15953f6bc
[Ahmet Ünsal] <[unsalahmet1998@outlook.com]>
2023-04-03 04:47:16 +0300

    test

--- Client/webpack.config.js
+++ Client/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
-  entry: path.resolve(__dirname, "./src/index.jsx"),
+  entry: path.resolve(__dirname, "./src/index.js"),
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.bundle.js",
